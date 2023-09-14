df155c5cac6579b0736c07582157aa14acefc9cc
taijulsir <princetaijul123@gmail.com>
2023-09-14 13:19:10 -0300

    created fake json data

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
     <>
       
       <h1 className='text-[#1C1B1B] text-[32px] font-bold text-center mt-[50px]'>Course Registration</h1>
-      <h1>check git bash
-      </h1>
+     
       
     </>
   )
